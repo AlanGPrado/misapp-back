0427c24a94398bf8ae47a2b73827875f5fc868da
Alan Prado <alan@semnexus.com>
2026-03-28 19:59:29 -0600

    refactor: update database connection SSL configuration to unconditionally reject unauthorized certificates

--- db/index.js
+++ db/index.js
@@ -6,9 +6,15 @@ dotenv.config();
 const { Pool } = pg;
 
 // You can use a connection string or individual parameters
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+// });
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export const query = (text, params) => pool.query(text, params);
