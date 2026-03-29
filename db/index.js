1ad117617d4ceaece0ce23543d10a09efa29764a
Alan Prado <alan@semnexus.com>
2026-03-28 20:12:08 -0600

    refactor: conditionally enable SSL for database connection based on environment

--- db/index.js
+++ db/index.js
@@ -6,15 +6,9 @@ dotenv.config();
 const { Pool } = pg;
 
 // You can use a connection string or individual parameters
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-// });
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 export const query = (text, params) => pool.query(text, params);
